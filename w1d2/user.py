6572918102e0ba6fc964287d8f6e81fd4bec81ee
connorbrennan1212 <connorbrennan1212@gmail.com>
2022-07-06 21:08:44 -0500

    oopos

--- w1d2/user.py
+++ w1d2/user.py
@@ -1,3 +1,5 @@
+# user.py chained
+
 class User:
     
     def __init__(self, first_name, last_name, email, age):
@@ -16,6 +18,7 @@ class User:
         print(f"Age: {self.age}")
         print(f"Rewards: {self.is_rewards_member}")
         print(f"Points: {self.gold_card_points}")
+        return self
 
     def enroll(self):
         if self.is_rewards_member == True:
@@ -23,19 +26,17 @@ class User:
             return False
         self.is_rewards_member = True
         self.gold_card_points = 200
+        return self
 
     def spend_points(self, amount):
         if self.gold_card_points < amount:
             print('Insufficient Funds')
             return self
         self.gold_card_points = self.gold_card_points - amount
+        return self
 
 my_user = User('Connor', 'Brennan', 'Connorbrennan1212@gmail.com', 26)
 
 
-my_user.display_info()
-my_user.enroll()
-my_user.display_info()
-my_user.spend_points(50)
-my_user.display_info()
+my_user.display_info().enroll().display_info().spend_points(50).display_info()
 
