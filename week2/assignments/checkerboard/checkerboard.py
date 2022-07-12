6eee80347ae193de844aafe6338e92a47cda0600
connorbrennan1212 <connorbrennan1212@gmail.com>
2022-07-12 13:38:22 -0500

    finished checkerboard

--- week2/assignments/checkerboard/checkerboard.py
+++ week2/assignments/checkerboard/checkerboard.py
@@ -17,5 +17,14 @@ def checkerboardwidth(x):
     color2='black'
     return render_template("index.html", x=x, y=y, color1=color1, color2 = color2)
 
+@app.route('/<int:x>/<int:y>')
+def checkerboardwandh(x,y):
+    x = int(x)
+    y = int(y)
+    color1='red'
+    color2='black'
+    return render_template("index.html", x=x, y=y, color1=color1, color2 = color2)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
