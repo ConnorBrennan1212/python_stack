cd78e0666091b2c7873fda0c52d133fbb219e404
connorbrennan1212 <connorbrennan1212@gmail.com>
2022-07-24 17:34:27 -0500

    USERCR

--- week2/assignments/usercrud/server.py
+++ week2/assignments/usercrud/server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return redirect('/users')
 
+@app.route('users')
+def index():
+    return render_template('show.html', users=User.get_all())
+
 if __name__=="__main__":
     app.run(debug=True)
--- week2/assignments/usercrud/user.py
+++ week2/assignments/usercrud/user.py
@@ -1,4 +1,4 @@
-from flask_app.config.mysqlconnection import connectToMySQL
+from mysqlconnection import connectToMySQL
 
 class User:
     def __init__(self, data):
@@ -8,3 +8,13 @@ class User:
         self.email = data['email']
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
+
+    @classmethod
+    def get_all(cls):
+        query = "SELECT * FROM users;"
+        results = connectToMySQL('usersschema').query_db(query)
+        users = []
+
+        for u in results:
+            users.append(cls(u))
+        return users
