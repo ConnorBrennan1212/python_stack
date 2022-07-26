1ccf40dfa191ecdb21b3390462fa5b3f97248343
connorbrennan1212 <connorbrennan1212@gmail.com>
2022-07-26 08:51:00 -0500

    register done

--- week2/assignments/Login_and_registration/flask_app/controllers/users_controller.py
+++ week2/assignments/Login_and_registration/flask_app/controllers/users_controller.py
@@ -1,7 +1,32 @@
 from flask_app import app
-from flask import render_template, request, redirect, session
+from flask import render_template, request, redirect, session, flash
 
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
+
+
+
+from flask_app.models.user_model import User
 
 @app.route('/')
 def index():
-    return "ok"
+    return render_template('index.html')
+
+@app.route('/users/register', methods=['POST'])
+def register_user():
+    #validate the data
+    if User.validate_user(request.form):
+        print("registration ok")
+        data = {
+            'first_name':request.form['first_name'],
+            'last_name':request.form['last_name'],
+            'email':request.form['email'],
+            'password':bcrypt.generate_password_hash(request.form['password']),
+        }
+        User.register_user(data)
+        flash('user registered, log in now')
+    else:
+        print('validation fails')
+    #create the user in database
+    return redirect('/')
--- week2/assignments/Login_and_registration/flask_app/models/user_model.py
+++ week2/assignments/Login_and_registration/flask_app/models/user_model.py
@@ -1,8 +1,54 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
-from flask_app import EMAIL_REGEX, DATABASE
+
+import re
+
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
 
 
 class User:
     def __init__(self,data):
-        self.first_name
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data ['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def register_user(cls, data):
+        query = 'INSERT INTO users (first_name, last_name, email, password) VALUES(%(first_name)s, %(last_name)s, %(email)s, %(password)s);'
+
+        result = connectToMySQL('login').query_db(query,data)
+        return result
+
+
+
+    @staticmethod
+    def validate_user(data):
+        is_valid = True
+        
+        #first_name
+        if len(data['first_name']) < 3 or len(data['first_name']) > 20:
+            is_valid = False
+            flash('first name must be between 2 and 20 characters')
+        #last_name
+        if len(data['last_name']) < 3 or len(data['last_name']) > 20:
+            is_valid = False
+            flash('last name must be between 2 and 20 characters')
+        #email
+        if not EMAIL_REGEX.match(data['email']):
+            is_valid = False
+            flash('email address must be in correct format')
+        #password
+        if len(data['password'])<8:
+            is_valid = False
+            flash('password must be at least 8 characters long')
+
+        if data['password'] != data['confirm_password']:
+            is_valid = False
+            flash('password and confirm password must match')
+
+        return is_valid
